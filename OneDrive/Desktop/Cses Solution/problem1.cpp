c6810d63efba65a2c4dba15cc8326308d58eba61
Shivanshu green Coder Singh <singhavinash232102@gmail.com>
2025-09-20 10:59:42 +0530

    Hi

--- OneDrive/Desktop/Cses Solution/problem1.cpp
+++ OneDrive/Desktop/Cses Solution/problem1.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+typedef long long ll;
+void solve(){
+  ll n;
+  cin>>n;
+  cout<<n<<" ";
+  while(n!=1){
+    if(n%2==0){
+      n/=2;
+      cout<<n<<" ";
+    }
+    else{
+      n=3*n+1;
+      cout<<n<<" ";
+    }
+  }
+}
+int main(){
+  solve();
+  return 0;
 }
